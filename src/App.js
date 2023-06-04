ff17e6878d447b135a834624049ff25e74a06af7
gogoend <gogoend@qq.com>
2023-06-04 17:06:37 +0000

    feat: 通过为每张图片加ref的方式，来实现“滚动到选中图片”

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 
 export default function CatFriends() {
   const [index, setIndex] = useState(0);
+  const refList = useRef(new Map());
+
   return (
     <>
       <nav>
-        <button onClick={() => {
-          if (index < catList.length - 1) {
-            setIndex(index + 1);
-          } else {
-            setIndex(0);
-          }
-        }}>
+        <button
+          onClick={() => {
+            if (index < catList.length - 1) {
+              setIndex(index + 1);
+              refList.current.get(index + 1).scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest',
+                inline: 'center'
+              });
+            } else {
+              setIndex(0);
+              refList.current.get(0).scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest',
+                inline: 'center'
+              });
+            }
+          }}
+        >
           Next
         </button>
       </nav>
       <div>
         <ul>
           {catList.map((cat, i) => (
-            <li key={cat.id}>
-              <img
-                className={
-                  index === i ?
-                    'active' :
-                    ''
+            <li
+              key={cat.id}
+              ref={(node) => {
+                if (node) {
+                  refList.current.set(i, node);
+                } else {
+                  refList.current.delete(i);
                 }
+              }}
+            >
+              <img
+                className={index === i ? "active" : ""}
                 src={cat.imageUrl}
-                alt={'Cat #' + cat.id}
+                alt={"Cat #" + cat.id}
               />
             </li>
           ))}
@@ -40,7 +59,6 @@ const catList = [];
 for (let i = 0; i < 10; i++) {
   catList.push({
     id: i,
-    imageUrl: 'https://placekitten.com/250/200?image=' + i
+    imageUrl: "https://placekitten.com/250/200?image=" + i,
   });
 }
-
